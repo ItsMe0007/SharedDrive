d5ba5346940677a7b0b8df1580f4fc2d57351ae9
Satyam Singh <satyam.singh@peoplestrong.com>
2024-12-20 00:19:15 +0530

    Adding Constant files

---
+++ Constant Files/EncashmentErrorConstants.java
@@ -0,0 +1,12 @@
+package com.peoplestrong.timeoff.encashment.constant;
+
+public interface EncashmentErrorConstants {
+
+    public static final String ENCASHMENTTASKDETAIL_API_NOTRESPONDING= "validate.encashmenttaskdetail.apinotresponding";
+
+    public static final String APPROVAL_HISTORY_API_ERROR ="validate.approvalhistory.apinotresponding";
+
+    public static final String FAILED_TOLOAD_SELECTED_TASKDETAILS = "validate.failed.toload.selected.taskdetails";
+
+    public static final String ENCASHMENTTASKDETAILS_API_NOTRESPONDING= "validate.encashmenttaskdetails.apinotresponding";
+}
---
+++ Constant Files/IEncashmentErrorConstant.java
@@ -0,0 +1,37 @@
+package com.peoplestrong.timeoff.encashment.constant;
+
+public class IEncashmentErrorConstant {
+    
+    public static final String NO_STAGE_ACTION_EXCEPTION = "validate.nostage.exception";
+
+    public static final String ENCASHMENT_TASK_CODE_START_CHARACTER = "E";
+    
+    public static final String SEQUENCE_FORMAT_PATTERN = "0000";
+
+    public static final String WORKFLOW_ROLE_ACTOR__ERROR = "Next stage Actor not found.";
+    
+    public static final String EMPLOYEE_ROLE = "Employee";
+    
+    public static final String L1_MANAGER_ROLE = "L1_MANAGER";
+
+    public static final String L2_MANAGER_ROLE = "L2_MANAGER";
+
+    public static final String HR_MANAGER_ROLE = "HR_MANAGER";
+
+    public static final String PRACTICE_HEAD_ROLE = "PRACTICE_HEAD";
+
+    public static final String FUNCTION_HEAD_ROLE = "FUNCTION_HEAD";
+
+    public static final String FUNCTIONAL_MANAGER_ROLE = "FUNCTIONAL_MANAGER";
+
+    public static final String ENCASHMENT_MODULE_NAME = "Encashment";
+    
+    public static final String LEAVEDURATIONTYPE_API_NOTRESPONDING = "validate.leavedurationtype.apinotresponding";
+    
+    public static class MessageReponse
+    {
+        public static final String SUCCESS = "EC200";
+
+        public static final String FAIL = "EC201";
+    }
+}
